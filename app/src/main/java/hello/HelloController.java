43c17b8cf4ec3216a6f6f97c2c1a2728ad963fd4
kolyaiks <nikolay.iks@gmail.com>
2021-11-20 23:59:37 +0300

    Update HelloController.java

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Spring Boot and kolyaiks!";
+        return "Hello from Spring Boot and kolyaiks - auto build and release!";
     }   
 }
